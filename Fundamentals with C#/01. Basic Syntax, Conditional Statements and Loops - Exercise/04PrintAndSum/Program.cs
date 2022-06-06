84c239c25edca6460a3c5b87866f2ea4656cfa23
MariaBanova <mariabanova2207@gmail.com>
2022-06-06 21:55:56 +0300

    Added a new task

---
+++ Fundamentals with C#/01. Basic Syntax, Conditional Statements and Loops - Exercise/04PrintAndSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var start = int.Parse(Console.ReadLine());   
+        var end = int.Parse(Console.ReadLine());
+
+        var TotalSum = 0;
+
+        for (int i = start; i <= end; i++)
+        {
+            TotalSum += i;
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();   
+        Console.WriteLine($"Sum: {TotalSum}");
+
+
+
+
+
+
+
+
+
+    }
+}
