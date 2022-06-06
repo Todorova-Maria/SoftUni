6cf5af02a5c677cfadd42a4c0f473c896c56bd6d
MariaBanova <mariabanova2207@gmail.com>
2022-06-06 21:53:29 +0300

    Added a new task

---
+++ Fundamentals with C#/01. Basic Syntax, Conditional Statements and Loops - Exercise/03Vacation/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        var type = Console.ReadLine();
+        var day = Console.ReadLine();
+
+        var totalmoney = 0.0;
+
+        
+       if (type.Equals("Students"))
+        {
+            if (day.Equals("Friday"))
+            {
+                totalmoney = num * 8.45;
+            } else if (day.Equals("Saturday"))
+            {
+                totalmoney = num * 9.80;
+            }
+            else if (day.Equals("Sunday"))
+            {
+                totalmoney = num * 10.46;
+            }
+            if (num >= 30)
+            {
+                totalmoney = totalmoney * 0.85;
+            }
+         
+        }
+
+        if (type.Equals("Business"))
+        {
+            if (day.Equals("Friday"))
+            {
+                totalmoney = num * 10.90;
+                if (num >= 100)
+                {
+                    totalmoney = (num - 10) * 10.90;
+                }
+            }
+            else if (day.Equals("Saturday"))
+            {
+                totalmoney = num * 15.60;
+                if (num >= 100)
+                {
+                    totalmoney = (num - 10) * 15.60;
+                }
+            }
+            else if (day.Equals("Sunday"))
+            {
+                totalmoney = num * 16.00;
+            
+                if (num >= 100)
+                {
+                    totalmoney = (num - 10) * 16.00;
+                }
+            }
+         
+        }
+
+        if (type.Equals("Regular"))
+        {
+            if (day.Equals("Friday"))
+            {
+                totalmoney = num * 15.00;
+            }
+            else if (day.Equals("Saturday"))
+            {
+                totalmoney = num * 20.00;
+            }
+            else if (day.Equals("Sunday"))
+            {
+                totalmoney = num * 22.50;
+            }
+            if (num >= 10 && num <= 20)
+            {
+                totalmoney = totalmoney * 0.95;
+            }
+
+        }
+        Console.WriteLine($"Total price: {totalmoney:F2}");
+
+    }
+}
