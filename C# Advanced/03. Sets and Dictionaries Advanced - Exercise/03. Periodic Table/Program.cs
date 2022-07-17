d43df9decd0fc5234cf054e3343249b31314b634
MariaBanova <mariabanova2207@gmail.com>
2022-07-17 23:02:02 +0300

    Add a new task.

---
+++ C# Advanced/03. Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        SortedSet<string> elements = new SortedSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] chemicalElements = Console.ReadLine().Split();
+
+            for (int j = 0; j < chemicalElements.Length; j++)
+            {
+                elements.Add(chemicalElements[j]); 
+            }
+           
+        }
+        Console.WriteLine(String.Join(" ", elements));
+    }
+}
