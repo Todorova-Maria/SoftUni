19f13651ff8d6a1f2b093ac1084730e37a8e9e7b
MariaBanova <mariabanova2207@gmail.com>
2022-07-28 13:18:59 +0300

    Added a new task.

---
+++ C# Advanced/05. Functional Programming - Exercise/07. PredicateforNames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int digit = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split();
+
+        Func<string, int, bool> nameLength = (name, digit) => name.Length <= digit;
+
+        string[] result = names.Where(name => nameLength(name, digit)).ToArray(); 
+
+        foreach (var name in result)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
