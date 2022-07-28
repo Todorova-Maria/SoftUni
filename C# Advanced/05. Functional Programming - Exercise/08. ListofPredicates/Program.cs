78c862b8471d52f0968f4132049d471bb50a0cfb
MariaBanova <mariabanova2207@gmail.com>
2022-07-28 13:20:42 +0300

    Added a new task.

---
+++ C# Advanced/05. Functional Programming - Exercise/08. ListofPredicates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        int[] divNumbers = Console.ReadLine().Split().Select(int.Parse).Distinct().ToArray();
+
+        int[] allNumbers = Enumerable.Range(1, number).ToArray();
+
+        List <Predicate<int>> predicateList = new List<Predicate<int>>(); 
+
+        foreach (var n in divNumbers)
+        {
+            predicateList.Add(x => x % n == 0);
+        }
+
+        foreach (var currentNum in allNumbers)
+        {
+            bool isDivisible = true; 
+
+            foreach (var predicate in predicateList)
+            {
+                if (!predicate(currentNum))
+                {
+                    isDivisible = false;
+                    break;
+                }
+            }
+            if(isDivisible)
+            {
+                Console.Write(currentNum + " ");
+            }
+        }
+    }
+}
