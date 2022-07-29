e88af3cf98a4cb0db7ec7461c9f03e63f9a5af86
MariaBanova <mariabanova2207@gmail.com>
2022-07-29 17:11:20 +0300

    Add a new task.

---
+++ C# Advanced/05. Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<string> names = Console.ReadLine().Split().ToList();
+        Func<string, int, bool> funcNames = (name, num) => name.Sum(x => x) >= num;
+        Func<List<string>, int, Func<string, int, bool>, string> desiredNames = (allNames, n, funcNames) =>
+        allNames.FirstOrDefault(x => funcNames(x, n));
+
+        string name = desiredNames(names, n, funcNames); 
+        Console.WriteLine(name);
+
+    }
+}
