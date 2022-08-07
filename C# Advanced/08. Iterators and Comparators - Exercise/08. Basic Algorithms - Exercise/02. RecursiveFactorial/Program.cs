3b673088dbeac4ad5bf346c8ddc3ad0cee1297cb
MariaBanova <mariabanova2207@gmail.com>
2022-08-07 21:29:34 +0300

    Added a new task.

---
+++ C# Advanced/08. Iterators and Comparators - Exercise/08. Basic Algorithms - Exercise/02. RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace RecursiveFactorial
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+
+        public static int Factorial(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+    }
+}
