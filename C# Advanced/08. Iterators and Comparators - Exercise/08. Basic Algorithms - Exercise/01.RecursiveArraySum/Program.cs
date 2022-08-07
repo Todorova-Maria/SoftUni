64079183823a6fd017b793a260829a2101c0bd8f
MariaBanova <mariabanova2207@gmail.com>
2022-08-07 21:29:00 +0300

    Added a new task.

---
+++ C# Advanced/08. Iterators and Comparators - Exercise/08. Basic Algorithms - Exercise/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace RecursiveArraySum
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Sum(arr, arr.Length - 1));
+        }
+
+        public static int Sum(int[] arr, int index)
+        {
+            if (index == 0)
+            {
+                return arr[0];
+            }
+
+            return arr[index] + Sum(arr, --index);
+        }
+    }
+}
