696cf1048c15fce616ac365c4bd480aa87962d91
MariaBanova <mariabanova2207@gmail.com>
2022-08-17 23:13:27 +0300

    Added a new task.

---
+++ C# Advanced/03. Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] sorted = nums.OrderByDescending(x => x).ToArray();
+
+        List<int> bestNums = new List<int>();
+        int count = 0;
+
+        for (int i = 0; i < sorted.Length; i++)
+        {
+            if (count < 3)
+            {
+                bestNums.Add(sorted[i]);
+                count++;
+            }
+        }
+        if (count == 3)
+        {
+            Console.WriteLine(String.Join(" ", bestNums));
+        }
+        else
+        {
+            Console.WriteLine(String.Join(" ", sorted));
+        }
+    }
+}
