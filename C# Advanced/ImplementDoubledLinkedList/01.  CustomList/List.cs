584336ee2c553bf0dc586b4ef7e8a1fde2d6cb30
MariaBanova <mariabanova2207@gmail.com>
2022-08-27 14:29:07 +0300

    Added a new task.

---
+++ C# Advanced/ImplementDoubledLinkedList/01.  CustomList/List.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoublyLinkedList
+{
+    public class List
+    {
+
+        private const int CountArray = 2;
+        private int[] items;
+
+        public List()
+        {
+            items = new int[CountArray];
+        }
+
+        public int Count { get; set; }
+        public int this[int i]  {
+            get
+            {
+                if(i < 0 || i >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return items[i]; 
+            }
+            set
+            {
+                items[i] = value;
+            }
+        }
+
+        public void Add (int element)
+        { 
+            if (Count == items.Length)
+            {
+                int[] temp = new int[items.Length * 2];
+                for (int i = 0; i < items.Length; i++)
+                {
+                    temp[i] = items[i];
+                }
+                items = temp;
+            }
+            items[Count++] = element;
+        }  
+
+        public int RemoveAt (int index)
+        { 
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            } 
+              int n = items[index];
+                items[index] = 0;
+
+            for (int i = index + 1; i < items.Length; i++)
+            {
+                    int tempN = items[i];
+                    items[i] = items[index];
+                    items[index] = tempN;
+                    index++;
+            }
+            Count--; 
+
+            if (Count <= items.Length / 4)
+            {
+                int[] tempArr = new int[items.Length/2];
+                for (int i = 0; i < tempArr.Length; i++)
+                {
+                    tempArr[i] = items[i];
+                }
+                items = tempArr;
+
+            }
+            return n;
+
+
+        } 
+
+        public bool Contains (int element)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == element)
+                {
+                    return true;
+                } 
+            }
+            return false;
+        }
+
+        public void Swap (int firstIndex, int secondIndex)
+        {
+            if(firstIndex < 0 || firstIndex >= Count || secondIndex < 0 || secondIndex >= Count)
+            {
+                throw new IndexOutOfRangeException(); 
+            }
+            int temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(String.Join(" ",items));
+        }
+    }
+}
---
+++ C# Advanced/ImplementDoubledLinkedList/01.  CustomList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace CustomDoublyLinkedList
+{
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+
+            list.Add(10);
+            list.Add(20);
+            list.Add(20);
+            list.Add(10);
+            list.Add(20);
+            list.Add(20);
+            list.Add(10);
+            list.Add(20);
+            list.Add(20);
+            list.RemoveAt(1);
+            list.RemoveAt(1);
+            list.RemoveAt(1);
+            list.RemoveAt(1);
+            bool isTrue = list.Contains(5);
+            list.Swap(0, 1);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            list.Print();
+            Console.WriteLine(list.Count);
+            Console.WriteLine(isTrue);
+        }
+    }
+}
