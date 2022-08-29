44742532affa10d5f13732112a2490bc87cfcea8
MariaBanova <mariabanova2207@gmail.com>
2022-08-29 21:47:04 +0300

    Added a new task.

---
+++ C# Advanced/08. Iterators and Comparators - Lab/03. Comparable Book/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            library.Add(new Book("Animal Farm", 2003, "George Orwell"));
+            library.Add(new Book("The Documents in the Case", 2003, "Dorothy Sayers", "Robert Eustace"));
+            library.Add(new Book("The Documents in the Case", 1930, "-"));
+
+            foreach (var book in library)
+            {
+                Console.WriteLine(book.ToString());
+            }
+        }
+    }
+}
