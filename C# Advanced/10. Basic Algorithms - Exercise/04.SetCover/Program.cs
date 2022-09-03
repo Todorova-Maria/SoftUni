5347cbf84dc1dd9f9ab131e65ff6830492cfca69
MariaBanova <mariabanova2207@gmail.com>
2022-09-03 23:49:43 +0300

    Added a new task.

---
+++ C# Advanced/10. Basic Algorithms - Exercise/04.SetCover/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SetCover
+{
+    public static void Main(string[] args)
+    {
+        int[] universe = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+        List<int[]> sets = new List<int[]>();
+          
+        
+        for (int i = 0; i < n; i++)
+        {
+            int[] currentSet = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            sets.Add(currentSet);
+        } 
+
+        List<int[]> selectedSets = ChooseSets(sets.ToList(), universe.ToList());
+        Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+
+        foreach (int[] set in selectedSets)
+        {
+            Console.WriteLine($"{{ {string.Join(", ", set)} }}");
+        }
+    }
+
+    public static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
+    {
+        List<int[]> selectedSets = new List<int[]>();
+
+        while (universe.Count != 0)
+        {
+            var currentSet = sets.OrderByDescending(set => set.Count(universe.Contains)).First();
+            selectedSets.Add(currentSet);
+            sets.Remove(currentSet);
+
+            foreach (var set in currentSet)
+            {
+                universe.Remove(set);
+            }
+        }
+        return selectedSets;
+    }
+}
