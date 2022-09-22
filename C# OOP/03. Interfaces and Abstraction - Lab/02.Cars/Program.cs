4f4d65eecc6c74a1b30375dc575ecd485c1d6dcf
MariaBanova <mariabanova2207@gmail.com>
2022-09-22 23:20:46 +0300

    Added a new task.

---
+++ C# OOP/03. Interfaces and Abstraction - Lab/02.Cars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class StartUp
+    { 
+        static void Main(string[] args)
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+
+            seat.Start();
+            seat.Stop(); 
+
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(tesla.ToString());
+
+        }
+    }
+}
---
+++ C# OOP/03. Interfaces and Abstraction - Lab/02.Cars/Tesla.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar, ICar
+    {
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public int Battery { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak!");
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} wiht {Battery} batteries";
+        }
+    }
+
+}
